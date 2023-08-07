8cdcecf36dbb614572ca22936a5ef5851f529424
Baptistemontan <baptiste.demontangon@gmail.com>
2023-08-08 00:17:21 +0200

    some small refactor

--- leptos_i18n/src/server/actix.rs
+++ leptos_i18n/src/server/actix.rs
@@ -1,10 +1,6 @@
 use crate::locale_traits::*;
-use actix_web::{http::header, FromRequest, ResponseError};
+use actix_web::http::header;
 use leptos::*;
-use std::{
-    fmt::Display,
-    future::{ready, Ready},
-};
 
 use crate::COOKIE_PREFERED_LANG;
 
@@ -12,62 +8,72 @@ pub fn fetch_locale_server<T: Locales>(cx: Scope) -> T::Variants {
     // when leptos_router inspect the routes it execute the code once but don't set an HttpRequest in the context,
     // so we can't expect it to be present.
     use_context::<actix_web::HttpRequest>(cx)
-        .map(|req| AcceptedLang::from_req(&req).0)
+        .map(|req| from_req(&req))
         .unwrap_or_default()
 }
 
-pub struct AcceptedLang<T: LocaleVariant>(pub T);
+fn from_req<T: LocaleVariant>(req: &actix_web::HttpRequest) -> T {
+    let prefered_lang = req
+        .cookie(COOKIE_PREFERED_LANG)
+        .and_then(|ck| T::from_str(ck.value()));
 
-impl<T: LocaleVariant> Default for AcceptedLang<T> {
-    fn default() -> Self {
-        AcceptedLang(Default::default())
+    if let Some(pref) = prefered_lang {
+        return pref;
     }
+
+    let Some(header) = req
+        .headers()
+        .get(header::ACCEPT_LANGUAGE)
+        .and_then(|header| header.to_str().ok())
+    else {
+        return Default::default();
+    };
+
+    let langs = crate::accepted_lang::parse_header(header);
+
+    LocaleVariant::find_locale(&langs)
 }
 
-impl<T: LocaleVariant> AcceptedLang<T> {
-    fn from_req(req: &actix_web::HttpRequest) -> Self {
-        let prefered_lang = req
-            .cookie(COOKIE_PREFERED_LANG)
-            .and_then(|ck| T::from_str(ck.value()));
+// use actix_web::{FromRequest, ResponseError};
+// use std::{
+//     fmt::Display,
+//     future::{ready, Ready},
+// };
 
-        if let Some(pref) = prefered_lang {
-            return AcceptedLang(pref);
-        }
+// pub struct AcceptedLang<T: LocaleVariant>(pub T);
 
-        let Some(header) = req
-            .headers()
-            .get(header::ACCEPT_LANGUAGE)
-            .and_then(|header| header.to_str().ok())
-        else {
-            return AcceptedLang::default();
-        };
+// impl<T: LocaleVariant> Default for AcceptedLang<T> {
+//     fn default() -> Self {
+//         AcceptedLang(Default::default())
+//     }
+// }
 
-        let langs = crate::accepted_lang::parse_header(header);
+// impl<T: LocaleVariant> AcceptedLang<T> {
+//     fn from_req(req: &actix_web::HttpRequest) -> Self {
+//         AcceptedLang(from_req(req))
+//     }
+// }
 
-        AcceptedLang(LocaleVariant::find_locale(&langs))
-    }
-}
+// impl<T: LocaleVariant> FromRequest for AcceptedLang<T> {
+//     type Error = Impossible;
 
-impl<T: LocaleVariant> FromRequest for AcceptedLang<T> {
-    type Error = Impossible;
+//     type Future = Ready<Result<Self, Self::Error>>;
 
-    type Future = Ready<Result<Self, Self::Error>>;
+//     fn from_request(
+//         req: &actix_web::HttpRequest,
+//         _payload: &mut actix_web::dev::Payload,
+//     ) -> Self::Future {
+//         ready(Ok(Self::from_req(req)))
+//     }
+// }
 
-    fn from_request(
-        req: &actix_web::HttpRequest,
-        _payload: &mut actix_web::dev::Payload,
-    ) -> Self::Future {
-        ready(Ok(Self::from_req(req)))
-    }
-}
+// #[derive(Debug)]
+// pub enum Impossible {}
 
-#[derive(Debug)]
-pub enum Impossible {}
+// impl Display for Impossible {
+//     fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         unreachable!()
+//     }
+// }
 
-impl Display for Impossible {
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        unreachable!()
-    }
-}
-
-impl ResponseError for Impossible {}
+// impl ResponseError for Impossible {}
--- leptos_i18n/src/server/axum.rs
+++ leptos_i18n/src/server/axum.rs
@@ -6,11 +6,11 @@ pub fn fetch_locale_server<T: Locales>(cx: Scope) -> T::Variants {
     // when leptos_router inspect the routes it execute the code once but don't set a RequestParts in the context,
     // so we can't expect it to be present.
     use_context::<leptos_axum::RequestParts>(cx)
-        .map(|req| from_req::<T>(&req))
+        .map(|req| from_req(&req))
         .unwrap_or_default()
 }
 
-fn from_req<T: Locales>(req: &leptos_axum::RequestParts) -> T::Variants {
+fn from_req<T: LocaleVariant>(req: &leptos_axum::RequestParts) -> T {
     if let Some(pref_lang_cookie) = get_prefered_lang_cookie::<T>(req) {
         return pref_lang_cookie;
     }
@@ -25,15 +25,15 @@ fn from_req<T: Locales>(req: &leptos_axum::RequestParts) -> T::Variants {
 
     let langs = crate::accepted_lang::parse_header(header);
 
-    LocaleVariant::find_locale(&langs)
+    T::find_locale(&langs)
 }
 
-fn get_prefered_lang_cookie<T: Locales>(req: &leptos_axum::RequestParts) -> Option<T::Variants> {
+fn get_prefered_lang_cookie<T: LocaleVariant>(req: &leptos_axum::RequestParts) -> Option<T> {
     req.headers
         .get_all(header::COOKIE)
         .into_iter()
         .filter_map(parse_cookie)
-        .filter_map(LocaleVariant::from_str)
+        .filter_map(T::from_str)
         .next()
 }
 
