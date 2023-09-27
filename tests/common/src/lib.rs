39f9de5252cb4e7d14debc55275109124d5d2c8c
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2023-09-27 19:00:35 +0200

    small refactor

--- tests/common/src/lib.rs
+++ tests/common/src/lib.rs
@@ -13,19 +13,18 @@ where
 }
 
 fn remove_noise<'a>(s: Oco<'a, str>, start_delim: &str, end_delim: &str) -> Oco<'a, str> {
-    if let Some((start, rest)) = s.split_once(start_delim) {
-        let mut output_str = start.to_owned();
-        let (_, mut s) = rest.split_once(end_delim).unwrap();
-        while let Some((start, rest)) = s.split_once(start_delim) {
-            output_str.push_str(start);
-            let (_, rest) = rest.split_once(end_delim).unwrap();
-            s = rest;
-        }
-        output_str.push_str(s);
-        Oco::Owned(output_str)
-    } else {
-        s
+    let Some((start, rest)) = s.split_once(start_delim) else {
+        return s;
+    };
+    let mut output_str = start.to_owned();
+    let (_, mut s) = rest.split_once(end_delim).unwrap();
+    while let Some((start, rest)) = s.split_once(start_delim) {
+        output_str.push_str(start);
+        let (_, rest) = rest.split_once(end_delim).unwrap();
+        s = rest;
     }
+    output_str.push_str(s);
+    Oco::Owned(output_str)
 }
 
 fn remove_html_comments(s: Oco<str>) -> Oco<str> {
@@ -46,19 +45,18 @@ fn split_html_special_char(s: &str) -> Option<(&str, char, &str)> {
 }
 
 fn decode_special_chars(s: Oco<str>) -> Oco<str> {
-    if let Some((before, ch, mut s)) = split_html_special_char(&s) {
-        let mut output_str = before.to_owned();
+    let Some((before, ch, mut s)) = split_html_special_char(&s) else {
+        return s;
+    };
+    let mut output_str = before.to_owned();
+    output_str.push(ch);
+    while let Some((before, ch, rest)) = split_html_special_char(s) {
+        output_str.push_str(before);
         output_str.push(ch);
-        while let Some((before, ch, rest)) = split_html_special_char(s) {
-            output_str.push_str(before);
-            output_str.push(ch);
-            s = rest;
-        }
-        output_str.push_str(s);
-        Oco::Owned(output_str)
-    } else {
-        s
+        s = rest;
     }
+    output_str.push_str(s);
+    Oco::Owned(output_str)
 }
 
 #[macro_export]
