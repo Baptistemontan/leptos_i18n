43df03f3ef6b49169a04db515de67fcde29775f2
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2023-09-27 17:17:17 +0200

    better test suite

--- tests/namespaces/src/lib.rs
+++ tests/namespaces/src/lib.rs
@@ -1,16 +1,45 @@
 #![deny(warnings)]
-
-pub mod app;
 leptos_i18n::load_locales!();
+#[cfg(test)]
+mod tests;
 
-#[cfg(feature = "hydrate")]
-#[wasm_bindgen::prelude::wasm_bindgen]
-pub fn hydrate() {
-    use app::App;
-    use leptos::*;
-    console_error_panic_hook::set_once();
+use leptos::{IntoView, Oco};
+pub fn render_to_string<'a, T: 'a>(view: T) -> Oco<'a, str>
+where
+    T: IntoView,
+{
+    let rendered = view.into_view().render_to_string();
+    let comment_removed = remove_html_comments(rendered);
+    remove_hk(comment_removed)
+}
 
-    leptos::mount_to_body(move || {
-        view! {  <App/> }
-    });
+fn remove_noise<'a>(s: Oco<'a, str>, start_delim: &str, end_delim: &str) -> Oco<'a, str> {
+    if let Some((start, rest)) = s.split_once(start_delim) {
+        let mut output_str = start.to_owned();
+        let (_, mut s) = rest.split_once(end_delim).unwrap();
+        while let Some((start, rest)) = s.split_once(start_delim) {
+            output_str.push_str(start);
+            let (_, rest) = rest.split_once(end_delim).unwrap();
+            s = rest;
+        }
+        output_str.push_str(s);
+        Oco::Owned(output_str)
+    } else {
+        s
+    }
+}
+
+pub fn remove_html_comments(s: Oco<str>) -> Oco<str> {
+    remove_noise(s, "<!--", "-->")
+}
+
+pub fn remove_hk(s: Oco<str>) -> Oco<str> {
+    remove_noise(s, " data-hk=\"", "\"")
+}
+
+#[macro_export]
+macro_rules! assert_eq_rendered {
+    ($left:expr, $($right:tt)*) => {
+        assert_eq!(render_to_string($left), $($right)*)
+    };
 }
