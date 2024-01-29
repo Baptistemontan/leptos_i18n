582eca9ebc405d31be7777c9eec9979c56fbb6a1
Baptiste <baptiste.demontangon@gmail.com>
2024-01-29 15:16:32 +0100

    fix test suite

--- tests/json/src/defaulted.rs
+++ tests/json/src/defaulted.rs
@@ -3,41 +3,41 @@ use common::*;
 
 #[test]
 fn defaulted_string() {
-    let en = td!(Locale::en, defaulted_string);
+    let en = td!(Locale::en, defaulted_string)();
     assert_eq!(en, "this string is declared in locale en");
-    let fr = td!(Locale::fr, defaulted_string);
+    let fr = td!(Locale::fr, defaulted_string)();
     assert_eq!(fr, "this string is declared in locale en");
 }
 
 #[test]
 fn defaulted_interpolation() {
-    let en = td!(Locale::en, defaulted_interpolation, locale = "en");
-    assert_eq_rendered!(en, "this interpolation is declared in locale en");
-    let fr = td!(Locale::fr, defaulted_interpolation, locale = "en");
-    assert_eq_rendered!(fr, "this interpolation is declared in locale en");
+    let en = td!(Locale::en, defaulted_interpolation, locale = "en")();
+    assert_eq_rendered!(en, "this interpolation is declared in locale en")();
+    let fr = td!(Locale::fr, defaulted_interpolation, locale = "en")();
+    assert_eq_rendered!(fr, "this interpolation is declared in locale en")();
 }
 
 #[test]
 fn defaulted_plurals() {
     let count = || 0;
-    let en = td!(Locale::en, defaulted_plurals, locale = "en", count);
+    let en = td!(Locale::en, defaulted_plurals, locale = "en", count)();
     assert_eq_rendered!(en, "zero");
-    let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count);
+    let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count)();
     assert_eq_rendered!(fr, "zero");
 
     for i in [-3, 5, 12] {
         let count = move || i;
-        let en = td!(Locale::en, defaulted_plurals, locale = "en", count);
+        let en = td!(Locale::en, defaulted_plurals, locale = "en", count)();
         assert_eq_rendered!(en, "this plural is declared in locale en");
-        let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count);
+        let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count)();
         assert_eq_rendered!(fr, "this plural is declared in locale en");
     }
 }
 
 #[test]
 fn defaulted_foreign_key() {
-    let en = td!(Locale::en, defaulted_foreign_key);
+    let en = td!(Locale::en, defaulted_foreign_key)();
     assert_eq_rendered!(en, "before Click to increment the counter after");
-    let fr = td!(Locale::fr, defaulted_foreign_key);
+    let fr = td!(Locale::fr, defaulted_foreign_key)();
     assert_eq_rendered!(fr, "before Click to increment the counter after");
 }
--- tests/yaml/src/defaulted.rs
+++ tests/yaml/src/defaulted.rs
@@ -3,33 +3,33 @@ use common::*;
 
 #[test]
 fn defaulted_string() {
-    let en = td!(Locale::en, defaulted_string);
+    let en = td!(Locale::en, defaulted_string)();
     assert_eq!(en, "this string is declared in locale en");
-    let fr = td!(Locale::fr, defaulted_string);
+    let fr = td!(Locale::fr, defaulted_string)();
     assert_eq!(fr, "this string is declared in locale en");
 }
 
 #[test]
 fn defaulted_interpolation() {
-    let en = td!(Locale::en, defaulted_interpolation, locale = "en");
+    let en = td!(Locale::en, defaulted_interpolation, locale = "en")();
     assert_eq_rendered!(en, "this interpolation is declared in locale en");
-    let fr = td!(Locale::fr, defaulted_interpolation, locale = "en");
+    let fr = td!(Locale::fr, defaulted_interpolation, locale = "en")();
     assert_eq_rendered!(fr, "this interpolation is declared in locale en");
 }
 
 #[test]
 fn defaulted_plurals() {
     let count = || 0;
-    let en = td!(Locale::en, defaulted_plurals, locale = "en", count);
+    let en = td!(Locale::en, defaulted_plurals, locale = "en", count)();
     assert_eq_rendered!(en, "zero");
-    let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count);
+    let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count)();
     assert_eq_rendered!(fr, "zero");
 
     for i in [-3, 5, 12] {
         let count = move || i;
-        let en = td!(Locale::en, defaulted_plurals, locale = "en", count);
+        let en = td!(Locale::en, defaulted_plurals, locale = "en", count)();
         assert_eq_rendered!(en, "this plural is declared in locale en");
-        let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count);
+        let fr = td!(Locale::fr, defaulted_plurals, locale = "en", count)();
         assert_eq_rendered!(fr, "this plural is declared in locale en");
     }
 }
