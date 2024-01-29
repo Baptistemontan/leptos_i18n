597ecc24cf830619294c2b64616c92893132b573
Baptiste <baptiste.demontangon@gmail.com>
2024-01-29 16:59:40 +0100

    fixed another error

--- leptos_i18n_macro/src/t_macro/interpolate.rs
+++ leptos_i18n_macro/src/t_macro/interpolate.rs
@@ -108,12 +108,12 @@ impl InterpolatedValue {
             }
             InterpolatedValue::AssignedVar { key, value } => {
                 let ts = (key.clone(), quote!(#value));
-                *self = InterpolatedValue::Var(std::mem::take(key));
+                *self = InterpolatedValue::Var(key.clone());
                 ts
             }
             InterpolatedValue::AssignedComp { key, value } => {
                 let ts = (key.clone(), quote!(#value));
-                *self = InterpolatedValue::Comp(std::mem::take(key));
+                *self = InterpolatedValue::Comp(key.clone());
                 ts
             }
             InterpolatedValue::DirectComp {
@@ -125,7 +125,7 @@ impl InterpolatedValue {
                     move |__children| leptos::view! { <#comp_name #attrs>{move || __children()}</#comp_name> };
                 };
                 let ts = (key.clone(), ts);
-                *self = InterpolatedValue::Comp(std::mem::take(key));
+                *self = InterpolatedValue::Comp(key.clone());
                 ts
             }
         }
