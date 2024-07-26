9e299b4ed4273019ea6c387cfada05a84ef3e3d9
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2024-07-27 01:56:23 +0200

    check english translations test for counter

--- examples/counter/e2e/example.spec.ts
+++ examples/counter/e2e/example.spec.ts
@@ -1,18 +1,94 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from "@playwright/test";
+import i18nEn from "../locales/en.json";
+import i18nFr from "../locales/fr.json";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.describe("when locale is the default locale (en-GB)", () => {
+  test("simple test", check_english);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("when locale is set to french (fr-FR)", () => {
+  test.use({
+    locale: "fr",
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test("simple test", check_french);
 });
+
+interface Input {
+  page: Page;
+}
+
+async function check_english({ page }: Input) {
+  await page.goto("/");
+
+  const lngButton = page.getByRole("button", {
+    name: i18nEn.click_to_change_lang,
+  });
+
+  await expect(lngButton).toBeVisible();
+
+  const incButton = page.getByRole("button", {
+    name: i18nEn.click_to_inc,
+  });
+
+  await expect(incButton).toBeVisible();
+
+  await expect(
+    page.getByText(i18nEn.click_count.replace("{{ count }}", "0"))
+  ).toBeVisible();
+
+  await incButton.click();
+
+  await expect(
+    page.getByText(i18nEn.click_count.replace("{{ count }}", "1"))
+  ).toBeVisible();
+
+  await lngButton.click();
+
+  await expect(
+    page.getByRole("button", {
+      name: i18nFr.click_to_change_lang,
+    })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("button", {
+      name: i18nFr.click_to_inc,
+    })
+  ).toBeVisible();
+  await expect(
+    page.getByText(i18nFr.click_count.replace("{{ count }}", "1"))
+  ).toBeVisible();
+}
+
+async function check_french({ page }: Input) {
+  // await page.goto("/");
+  // const lngButton = page.getByRole("button", {
+  //   name: i18nFr.click_to_change_lang,
+  // });
+  // await expect(lngButton).toBeVisible();
+  // const incButton = page.getByRole("button", {
+  //   name: i18nFr.click_to_inc,
+  // });
+  // await expect(incButton).toBeVisible();
+  // await expect(
+  //   page.getByText(i18nFr.click_count.replace("{{ count }}", "0"))
+  // ).toBeVisible();
+  // await incButton.click();
+  // await expect(
+  //   page.getByText(i18nFr.click_count.replace("{{ count }}", "1"))
+  // ).toBeVisible();
+  // await lngButton.click();
+  // await expect(
+  //   page.getByRole("button", {
+  //     name: i18nEn.click_to_change_lang,
+  //   })
+  // ).toBeVisible();
+  // await expect(
+  //   page.getByRole("button", {
+  //     name: i18nEn.click_to_inc,
+  //   })
+  // ).toBeVisible();
+  // await expect(
+  //   page.getByText(i18nEn.click_count.replace("{{ count }}", "1"))
+  // ).toBeVisible();
+}
