d43a4588545c3f0625e8efc82d28e86fbad08b72
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2024-07-27 06:01:08 +0200

    check cookie in e2e

--- examples/counter/e2e/example.spec.ts
+++ examples/counter/e2e/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import i18nEn from "../locales/en.json";
 import i18nFr from "../locales/fr.json";
 
@@ -14,12 +14,21 @@ test.describe("when locale is set to french (fr-FR)", () => {
   test("simple test", check_french);
 });
 
-interface Input {
-  page: Page;
+type TestArgs = Parameters<Parameters<typeof test>[2]>[0]; // wonky
+
+async function check_cookie(
+  context: TestArgs["context"],
+  name: string,
+  value: string
+) {
+  const cookies = await context.cookies();
+  await expect(cookies.find((c) => c.name == name)?.value).toBe(value);
 }
 
-async function check_english({ page }: Input) {
+async function check_english({ page, context }: TestArgs) {
   await page.goto("/");
+
+  await check_cookie(context, "COOKIE_PREFERED_LANG", "en");
 
   const lngButton = page.getByRole("button", {
     name: i18nEn.click_to_change_lang,
@@ -43,6 +52,8 @@ async function check_english({ page }: Input) {
     page.getByText(i18nEn.click_count.replace("{{ count }}", "1"))
   ).toBeVisible();
 
+  // switch to french
+
   await lngButton.click();
 
   await expect(
@@ -58,26 +69,40 @@ async function check_english({ page }: Input) {
   await expect(
     page.getByText(i18nFr.click_count.replace("{{ count }}", "1"))
   ).toBeVisible();
+
+  await check_cookie(context, "COOKIE_PREFERED_LANG", "fr");
 }
 
-async function check_french({ page }: Input) {
+async function check_french({ page, context }: TestArgs) {
   await page.goto("/");
+
+  await check_cookie(context, "COOKIE_PREFERED_LANG", "en");
+
   const lngButton = page.getByRole("button", {
     name: i18nFr.click_to_change_lang,
   });
+
   await expect(lngButton).toBeVisible();
+
   const incButton = page.getByRole("button", {
     name: i18nFr.click_to_inc,
   });
+
   await expect(incButton).toBeVisible();
+
   await expect(
     page.getByText(i18nFr.click_count.replace("{{ count }}", "0"))
   ).toBeVisible();
+
   await incButton.click();
+
   await expect(
     page.getByText(i18nFr.click_count.replace("{{ count }}", "1"))
   ).toBeVisible();
+
+  // switch to english
   await lngButton.click();
+
   await expect(
     page.getByRole("button", {
       name: i18nEn.click_to_change_lang,
@@ -91,4 +116,6 @@ async function check_french({ page }: Input) {
   await expect(
     page.getByText(i18nEn.click_count.replace("{{ count }}", "1"))
   ).toBeVisible();
+
+  await check_cookie(context, "COOKIE_PREFERED_LANG", "fr");
 }
