774ceb4cda65a1d2fc43cfcda7c73ac2ce9e59eb
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2024-07-29 03:35:08 +0200

    I18n test fixture init

--- examples/utils/src/index.ts
+++ examples/utils/src/index.ts
@@ -1,7 +1,19 @@
-import { test, expect, Page } from "@playwright/test";
+import {
+  test,
+  expect,
+  Page,
+  Fixtures,
+  TestType,
+  TestFixture,
+  PlaywrightWorkerArgs,
+  PlaywrightWorkerOptions,
+  PlaywrightTestOptions,
+  PlaywrightTestArgs,
+} from "@playwright/test";
 import * as os from "os";
 
-export type TestArgs = Parameters<Parameters<typeof test>[2]>[0]; // wonky
+type TestParams = Parameters<Parameters<typeof test>[2]>;
+export type TestArgs = TestParams[0];
 export type BrowserName = TestArgs["browserName"];
 
 const WIN = os.platform() == "win32";
@@ -9,4 +21,30 @@ const WIN = os.platform() == "win32";
 export function fail_windows_webkit({ browserName }: TestArgs): boolean {
   const WEBKIT = browserName === "webkit";
   return WEBKIT && WIN;
+}
+
+class I18n {}
+
+type TFunc = any;
+
+export type I18nFixture = {
+  i18n: I18n;
+  t: TFunc;
+};
+
+function createI18nFixture(): Fixtures<
+  I18nFixture,
+  PlaywrightWorkerArgs & PlaywrightWorkerOptions,
+  PlaywrightTestArgs & PlaywrightTestOptions,
+  PlaywrightWorkerArgs & PlaywrightWorkerOptions
+> {
+  return {
+    i18n: async ({}, use) => {
+      const i18n = new I18n();
+      await use(i18n);
+    },
+    t: async ({ i18n }, use) => {
+      await use({});
+    },
+  };
 }
