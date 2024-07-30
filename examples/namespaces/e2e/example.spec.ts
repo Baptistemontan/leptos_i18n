0f9dbb733a58e48a65fd37ac1d32d9ab7a9f83b6
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2024-07-30 04:08:35 +0200

    namespaces example e2e tests

--- examples/namespaces/e2e/example.spec.ts
+++ examples/namespaces/e2e/example.spec.ts
@@ -1,18 +1,131 @@
-import { test, expect } from '@playwright/test';
+import { test as base, expect, Page } from "@playwright/test";
+import i18nClEn from "#locales/en/change_locale.json";
+import i18nClFr from "#locales/fr/change_locale.json";
+import i18nCounterEn from "#locales/en/counter.json";
+import i18nCounterFr from "#locales/fr/counter.json";
+import { fail_windows_webkit, createI18nFixture } from "../../utils";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const LNG_BUTTON_XPATH = "xpath=//html/body/button[1]";
+const INC_BUTTON_XPATH = "xpath=//html/body/button[2]";
+const COUNTER_XPATH = "xpath=//html/body/p";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+const test = base.extend(
+  createI18nFixture({
+    default_locale: "en",
+    namespaces: {
+      change_locale: {
+        en: i18nClEn,
+        fr: i18nClFr,
+      },
+      counter: {
+        en: i18nCounterEn,
+        fr: i18nCounterFr,
+      },
+    },
+  })
+);
+
+type I18n = Parameters<Parameters<typeof test>[2]>[0]["i18n"];
+
+async function switch_lang(i18n: I18n) {
+  if (i18n.locale == "en") {
+    await i18n.set_locale("fr");
+  } else {
+    await i18n.set_locale("en");
+  }
+}
+
+test.skip(fail_windows_webkit, "webkit does not support wasm on windows");
+
+test.beforeEach(async ({ i18n, page }) => {
+  i18n.on_locale_change(async () => {
+    await page.locator(LNG_BUTTON_XPATH).click();
+  });
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test.afterEach(async ({ context }) => {
+  await context.clearCookies();
 });
+
+test.describe("when locale is the default locale (en-GB)", () => {
+  test("check counter", ({ page, i18n }) => check_counter(page, i18n));
+  test("check lang switch", ({ page, i18n }) => check_lang_switch(page, i18n));
+  test("check state keeping", ({ page, i18n }) =>
+    check_state_keeping(page, i18n));
+});
+
+test.describe("when locale is set to french (fr-FR)", () => {
+  test.use({
+    locale: "fr-FR",
+  });
+
+  test("check counter", ({ page, i18n }) => check_counter(page, i18n));
+  test("check lang switch", ({ page, i18n }) => check_lang_switch(page, i18n));
+  test("check state keeping", ({ page, i18n }) =>
+    check_state_keeping(page, i18n));
+});
+
+async function check_counter(
+  page: Page,
+  i18n: I18n,
+  load_page: boolean = true
+) {
+  if (load_page) {
+    await page.goto("/");
+  }
+
+  await expect(page.locator(LNG_BUTTON_XPATH)).toHaveText(
+    i18n.t("change_locale.click_to_change_lang")
+  );
+  await expect(page.locator(INC_BUTTON_XPATH)).toHaveText(
+    i18n.t("counter.click_to_inc")
+  );
+
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 0 })
+  );
+  await page.locator(INC_BUTTON_XPATH).click();
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 1 })
+  );
+  await page.locator(INC_BUTTON_XPATH).click();
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 2 })
+  );
+  await page.locator(INC_BUTTON_XPATH).click();
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 3 })
+  );
+}
+
+async function check_lang_switch(page: Page, i18n: I18n) {
+  await page.goto("/");
+
+  await switch_lang(i18n);
+
+  await check_counter(page, i18n, false);
+  // check if locale persist
+  await page.reload();
+  await check_counter(page, i18n, false);
+}
+
+async function check_state_keeping(page: Page, i18n: I18n) {
+  await page.goto("/");
+
+  await page.locator(INC_BUTTON_XPATH).click({ clickCount: 3 });
+
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 3 })
+  );
+
+  await switch_lang(i18n);
+
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 3 })
+  );
+
+  await page.locator(INC_BUTTON_XPATH).click();
+  await expect(page.locator(COUNTER_XPATH)).toHaveText(
+    i18n.t("counter.click_count", { count: 4 })
+  );
+}
