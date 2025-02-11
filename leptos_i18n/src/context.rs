8bb8dd9e0f113221d3e222c79f4ec037eb408ff2
Baptistemontan <baptiste.demontangon@gmail.com>
2025-02-11 02:36:09 +0100

    function to resolve the locale non-reactively

--- leptos_i18n/src/context.rs
+++ leptos_i18n/src/context.rs
@@ -188,6 +188,41 @@ impl<L: Locale> Default for I18nContextOptions<'_, L> {
     }
 }
 
+/// Same as `resolve_locale` but with some cookies options.
+pub fn resolve_locale_with_options<L: Locale>(options: I18nContextOptions<L>) -> L {
+    let I18nContextOptions {
+        enable_cookie,
+        cookie_name,
+        cookie_options,
+        ssr_lang_header_getter,
+    } = options;
+    let (lang_cookie, _) = if ENABLE_COOKIE && enable_cookie {
+        leptos_use::use_cookie_with_options::<L, FromToStringCodec>(&cookie_name, cookie_options)
+    } else {
+        let (lang_cookie, set_lang_cookie) = signal(None);
+        (lang_cookie.into(), set_lang_cookie)
+    };
+    fetch_locale::resolve_locale(lang_cookie.get_untracked(), ssr_lang_header_getter)
+}
+
+/// Resolve the locale.
+///
+/// This as the same behavior as calling `init_i18n_context().get_locale_untracked()`.
+///
+/// This function primary usage is to access a user locale in a server function, but is not constrained to it.
+///
+/// Here is the list of detection methods, sorted in priorities:
+/// 1. The "lang" attribute is set on the `<html>` element in hydrate
+/// 1. A cookie is present that contains a previously detected locale
+/// 1. A locale can be matched based on the [`Accept-Language` header](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language) in SSR
+/// 1. A locale can be matched based on the [`navigator.languages` API](https://developer.mozilla.org/en-US/docs/Web/API/Navigator/languages) in CSR
+/// 1. As a last resort, the default locale is used.
+///
+/// *note*: this function does not take into account URL locale prefix when using `I18nRoute` (e.g. `/en/about`)
+pub fn resolve_locale<L: Locale>() -> L {
+    resolve_locale_with_options(Default::default())
+}
+
 /// Same as `init_i18n_context` but with some cookies options.
 #[track_caller]
 pub fn init_i18n_context_with_options<L: Locale>(options: I18nContextOptions<L>) -> I18nContext<L> {
@@ -209,6 +244,7 @@ pub fn init_i18n_context_with_options<L: Locale>(options: I18nContextOptions<L>)
 
     init_context_inner::<L>(set_lang_cookie, initial_locale)
 }
+
 /// Initialize a `I18nContext` without providing it.
 #[track_caller]
 pub fn init_i18n_context<L: Locale>() -> I18nContext<L> {
