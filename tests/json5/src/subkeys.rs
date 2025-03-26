968fc925ceceacac94566a0e305d0268176d5cdc
Saber Haj Rabiee <saber.hajrabiee@gmail.com>
2025-03-26 02:45:04 -0700

    fix: remove unnecessary into_any() in json5

--- tests/json5/src/subkeys.rs
+++ tests/json5/src/subkeys.rs
@@ -11,7 +11,7 @@ fn subkey_1() {
 
 #[test]
 fn subkey_2() {
-    let b = |children: ChildrenFn| view! { <b>{move || children()}</b> }.into_any();
+    let b = |children: ChildrenFn| view! { <b>{move || children()}</b> };
     let en = td!(Locale::en, subkeys.subkey_2, <b>);
     assert_eq_rendered!(en, "<b>subkey_2</b>");
     let fr = td!(Locale::fr, subkeys.subkey_2, <b>);
