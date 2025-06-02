4a8a9b480c28eab68ffc52d5d68a1bf492abec31
Baptistemontan <baptiste.demontangon@gmail.com>
2025-06-03 00:00:44 +0200

    simple search and replace

--- examples/dynamic_load/namespaces/build.rs
+++ examples/dynamic_load/namespaces/build.rs
@@ -5,14 +5,11 @@ fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=Cargo.toml");
 
-    let mod_directory = PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("baked_data");
     let i18n_mod_directory = PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("i18n");
 
     let translations_infos = TranslationsInfos::parse().unwrap();
 
     translations_infos.rerun_if_locales_changed();
-
-    translations_infos.generate_data(mod_directory).unwrap();
 
     translations_infos
         .generate_i18n_module(i18n_mod_directory, false)
