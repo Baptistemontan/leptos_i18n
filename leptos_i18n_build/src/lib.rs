93340a026045dd03fa523081d27d931a642b773a
Baptistemontan <baptiste.de-montangon@ledger.fr>
2025-06-03 10:15:04 +0200

    format the generated code if in debug mode

--- leptos_i18n_build/src/lib.rs
+++ leptos_i18n_build/src/lib.rs
@@ -243,6 +243,13 @@ impl TranslationsInfos {
     ) -> Result<()> {
         let ts = leptos_i18n_codegen::gen_code(&self.parsed_locales, None, interpolate_display)?;
 
+        let ts_as_string = if cfg!(debug_assertions) {
+            let as_file = syn::parse_quote!(#ts);
+            prettyplease::unparse(&as_file)
+        } else {
+            ts.to_string()
+        };
+
         create_dir_all(&mod_directory)?;
 
         mod_directory.push("mod.rs");
@@ -250,7 +257,7 @@ impl TranslationsInfos {
         let mut file = File::create(&mod_directory)?;
 
         use std::io::Write;
-        write!(&mut file, "{ts}")?;
+        write!(&mut file, "{ts_as_string}")?;
 
         Ok(())
     }
