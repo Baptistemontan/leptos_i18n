5398877319dc457e2b659cf3e655fdd95752f537
Baptistemontan <baptiste.de-montangon@ledger.fr>
2025-06-05 11:07:22 +0200

    allow custom parser for file format

--- leptos_i18n_parser/src/parse_locales/options.rs
+++ leptos_i18n_parser/src/parse_locales/options.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::{fmt::Debug, io::Read, path::Path, sync::Arc};
 
 use crate::parse_locales::locale::{Locale, LocaleSeed, SerdeError};
 
@@ -11,13 +11,25 @@ pub struct Options {
     pub show_keys_only: bool,
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Clone, Default)]
 #[non_exhaustive]
 pub enum FileFormat {
     #[default]
     Json,
     Json5,
     Yaml,
+    Custom(Arc<dyn Parser>),
+}
+
+impl Debug for FileFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FileFormat::Json => f.write_str("Json"),
+            FileFormat::Json5 => f.write_str("Json5"),
+            FileFormat::Yaml => f.write_str("Yaml"),
+            FileFormat::Custom(..) => f.debug_tuple("Custom").finish(),
+        }
+    }
 }
 
 impl Default for Options {
@@ -37,53 +49,63 @@ impl Options {
         }
     }
 
-    pub const fn file_format(self, file_format: FileFormat) -> Self {
+    pub fn file_format(self, file_format: FileFormat) -> Self {
         Self {
             file_format,
             ..self
         }
     }
 
-    pub const fn suppress_key_warnings(self, suppress_key_warnings: bool) -> Self {
+    pub fn suppress_key_warnings(self, suppress_key_warnings: bool) -> Self {
         Self {
             suppress_key_warnings,
             ..self
         }
     }
 
-    pub const fn interpolate_display(self, interpolate_display: bool) -> Self {
+    pub fn interpolate_display(self, interpolate_display: bool) -> Self {
         Self {
             interpolate_display,
             ..self
         }
     }
 
-    pub const fn show_keys_only(self, show_keys_only: bool) -> Self {
+    pub fn show_keys_only(self, show_keys_only: bool) -> Self {
         Self {
             show_keys_only,
+            ..self
+        }
+    }
+
+    pub fn with_custom_parser<P: Parser>(self, parser: P) -> Self {
+        Self {
+            file_format: FileFormat::Custom(Arc::new(parser)),
             ..self
         }
     }
 }
 
 impl FileFormat {
-    pub const fn get_files_exts(&self) -> &'static [&'static str] {
+    pub fn get_files_exts(&self) -> &'static [&'static str] {
         match self {
             FileFormat::Json => &["json"],
             FileFormat::Json5 => &["json5"],
             FileFormat::Yaml => &["yaml", "yml"],
+            FileFormat::Custom(parser) => parser.file_extensions(),
         }
     }
 
     pub fn deserialize<R: Read>(
         &self,
-        locale_file: R,
+        mut locale_file: R,
+        path: &Path,
         seed: LocaleSeed,
     ) -> Result<Locale, SerdeError> {
         match self {
             FileFormat::Json => de_json(locale_file, seed),
             FileFormat::Json5 => de_json5(locale_file, seed),
             FileFormat::Yaml => de_yaml(locale_file, seed),
+            FileFormat::Custom(parser) => parser.deserialize(&mut locale_file, path, seed),
         }
     }
 }
@@ -103,4 +125,15 @@ fn de_json5<R: Read>(mut locale_file: R, seed: LocaleSeed) -> Result<Locale, Ser
 fn de_yaml<R: Read>(locale_file: R, seed: LocaleSeed) -> Result<Locale, SerdeError> {
     let deserializer = serde_yaml::Deserializer::from_reader(locale_file);
     serde::de::DeserializeSeed::deserialize(seed, deserializer).map_err(SerdeError::Yaml)
+}
+
+pub trait Parser: 'static {
+    fn deserialize(
+        &self,
+        reader: &mut dyn Read,
+        path: &Path,
+        seed: LocaleSeed,
+    ) -> Result<Locale, SerdeError>;
+
+    fn file_extensions(&self) -> &'static [&'static str];
 }
