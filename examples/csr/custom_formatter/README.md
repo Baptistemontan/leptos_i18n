7ae823c474a26d1a152b017379e39e419d96201b
Baptistemontan <baptiste.demontangon@gmail.com>
2026-01-02 21:29:56 +0100

    custom formatter example

---
+++ examples/csr/custom_formatter/README.md
@@ -0,0 +1,11 @@
+# Interpolation Example
+
+This example showcase how you can interpolate components/variable in your translations.
+
+## How to run
+
+Simply use `trunk` to run it:
+
+```bash
+trunk serve --open
+```
---
+++ examples/csr/custom_formatter/src/app.rs
@@ -0,0 +1,36 @@
+use crate::i18n::*;
+use leptos::prelude::*;
+
+#[component]
+#[allow(non_snake_case)]
+pub fn App() -> impl IntoView {
+    leptos_meta::provide_meta_context();
+
+    view! {
+        <I18nContextProvider>
+            <Component />
+        </I18nContextProvider>
+    }
+}
+
+#[component]
+#[allow(non_snake_case)]
+pub fn Component() -> impl IntoView {
+    let i18n = use_i18n();
+
+    let on_switch = move |_| {
+        let new_lang = match i18n.get_locale() {
+            Locale::en => Locale::fr,
+            Locale::fr => Locale::en,
+        };
+        i18n.set_locale(new_lang);
+    };
+
+    view! {
+        <div style="white-space: pre-wrap">
+            {t!(i18n, custom_formatter, var = || "test")}
+            <br/>
+            <button on:click=on_switch>{t!(i18n, click_to_change_lang)}</button>
+        </div>
+    }
+}
