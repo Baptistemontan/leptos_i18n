c65b1dc91c3a562c61cb97d69959f35adeee2022
Baptistemontan <baptiste.demontangon@gmail.com>
2026-04-27 18:36:27 +0200

    fix custom_formatter example build script

--- examples/csr/custom_formatter/build.rs
+++ examples/csr/custom_formatter/build.rs
@@ -1,6 +1,7 @@
 use leptos_i18n_build::{
     formatter::{Formatter, FormatterToTokens, Key},
-    Config, ParseOptions, TranslationsInfos,
+    options::ParseOptions,
+    Config, TranslationsInfos,
 };
 use proc_macro2::TokenStream;
 use quote::quote;
@@ -13,9 +14,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let i18n_mod_directory = PathBuf::from(std::env::var_os("OUT_DIR").unwrap()).join("i18n");
 
-    let options = ParseOptions::new()
-        .interpolate_display(true)
-        .add_formatter(PaddingFormatterParser);
+    let options = ParseOptions::new().add_formatter(PaddingFormatterParser);
+    let codegen_options = CodegenOptions::default().interpolate_display(true);
 
     let cfg = Config::new("en")?.add_locale("fr")?.parse_options(options);
 
@@ -25,7 +25,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     translations_infos.rerun_if_locales_changed();
 
-    translations_infos.generate_i18n_module(i18n_mod_directory)?;
+    translations_infos.generate_i18n_module_with_options(i18n_mod_directory, codegen_options)?;
 
     Ok(())
 }
