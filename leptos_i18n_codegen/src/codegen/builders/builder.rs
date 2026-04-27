56f57971a53121fb4d58998331c440e122315d98
Baptistemontan <baptiste.demontangon@gmail.com>
2026-04-27 14:25:02 +0200

    extract common, builder logic into a function

--- leptos_i18n_codegen/src/codegen/builders/builder.rs
+++ leptos_i18n_codegen/src/codegen/builders/builder.rs
@@ -8,7 +8,7 @@ pub fn gen_builder(infos: &BuilderInfos, markers_field: &syn::Ident) -> TokenStr
     let empty = {
         let iter = infos.fields.iter().map(|_| quote!(()));
 
-        quote!(#(#iter,)*)
+        quote!((#(#iter,)*))
     };
 
     let methods = gen_fields_methods(&infos.fields);
@@ -37,11 +37,11 @@ pub fn gen_builder(infos: &BuilderInfos, markers_field: &syn::Ident) -> TokenStr
     });
 
     quote! {
-        pub struct Builder<Fields = (#empty)>(Fields);
+        pub struct Builder<Fields = #empty>(Fields);
 
         impl Builder {
             pub const fn new() -> Self {
-                Builder((#empty))
+                Builder(#empty)
             }
         }
 
@@ -74,43 +74,8 @@ fn gen_var_methods(
     before: &[Field],
     after: &[Field],
 ) -> TokenStream {
-    let before_generics = before.iter().map(|field| &field.generic);
-    let after_generics = after.iter().map(|field| &field.generic);
-    let generics = {
-        let iter = before_generics.clone().chain(after_generics.clone());
-        quote!(#(#iter,)*)
-    };
-
     let key = &field.key;
     let key_generic = &field.generic;
-
-    let destructured_before = before.iter().map(|f| &*f.key.ident);
-    let destructured_after = after.iter().map(|f| &*f.key.ident);
-
-    let destructured = {
-        let (destructured_before, destructured_after) =
-            (destructured_before.clone(), destructured_after.clone());
-
-        quote! {
-            let (#(#destructured_before,)* (), #(#destructured_after,)*) = self.0;
-        }
-    };
-
-    let destructured_dup = quote! {
-        let (#(#destructured_before,)* (_,), #(#destructured_after,)*) = self.0;
-    };
-
-    let constructed = {
-        let iter = before
-            .iter()
-            .chain(Some(field))
-            .chain(after)
-            .map(|f| &*f.key.ident);
-
-        quote! {
-            Builder((#(#iter,)*))
-        }
-    };
 
     let var_name = key
         .name
@@ -120,64 +85,26 @@ fn gen_var_methods(
     let repeated_message = format!("Repeated variable {var_name}");
     let missing_message = format!("Missing variable {var_name}");
 
-    let right_generics = {
-        let (before_generics, after_generics) = (before_generics.clone(), after_generics.clone());
-
-        quote! {
-            ( #(#before_generics,)* (), #(#after_generics,)*)
-        }
-    };
-
-    let dup_right_generics = {
-        let (before_generics, after_generics) = (before_generics.clone(), after_generics.clone());
-
-        quote! {
-            ( #(#before_generics,)* (__Dup__,), #(#after_generics,)*)
-        }
-    };
-
-    let build_right_generics = {
-        let (before_generics, after_generics) = (before_generics.clone(), after_generics.clone());
-
-        quote! {
-            ( #((#before_generics,),)* (), #(#after_generics,)*)
-        }
-    };
-
-    let constructed_out_generics = {
-        quote! {
-            ( #(#before_generics,)* (#key_generic,), #(#after_generics,)*)
-        }
-    };
-
     let bounded_generics = VarOrComp::get_bounded_var_generics(&field.generic, bounds, plural);
 
-    quote! {
-        impl<#generics> Builder<#right_generics> {
-            pub fn #key<#bounded_generics>(self, #key: #key_generic) -> Builder<#constructed_out_generics> {
-                let #key = (#key,);
-                #destructured
-                #constructed
-            }
-        }
+    let constructed_out_generics = quote! {
+        #key_generic
+    };
 
-        impl<__Dup__, #generics> Builder<#dup_right_generics> {
-            #[deprecated(note = #repeated_message)]
-            pub fn #key<#bounded_generics>(self, #key: #key_generic) -> Builder<#constructed_out_generics> {
-                let #key = (#key,);
-                #destructured_dup
-                #constructed
-            }
-        }
+    let constructed = quote! {
+        #key
+    };
 
-        impl<#generics> Builder<#build_right_generics> {
-            #[deprecated(note = #missing_message)]
-            pub fn build(self) -> ! {
-                panic!()
-            }
-        }
-
-    }
+    gen_methods_inner(
+        field,
+        before,
+        after,
+        &constructed,
+        &constructed_out_generics,
+        &bounded_generics,
+        &repeated_message,
+        &missing_message,
+    )
 }
 
 fn gen_comp_methods(
@@ -186,6 +113,53 @@ fn gen_comp_methods(
     self_closed: bool,
     before: &[Field],
     after: &[Field],
+) -> TokenStream {
+    let key = &field.key;
+    let key_generic = &field.generic;
+
+    let var_name = key
+        .name
+        .strip_prefix("comp_")
+        .expect("components keys must start with var_");
+
+    let repeated_message = format!("Repeated component {var_name}");
+    let missing_message = format!("Missing component {var_name}");
+
+    let [bounded_generics, into_view_bounded_generics] =
+        VarOrComp::get_bounded_comp_generics(&field.generic, into_view, self_closed);
+    let bounded_generics = quote! {
+        #bounded_generics, #into_view_bounded_generics
+    };
+
+    let constructed_out_generics = quote! {
+        (#key_generic, core::marker::PhantomData<#into_view>)
+    };
+
+    let constructed = quote! {
+        (#key, core::marker::PhantomData)
+    };
+
+    gen_methods_inner(
+        field,
+        before,
+        after,
+        &constructed,
+        &constructed_out_generics,
+        &bounded_generics,
+        &repeated_message,
+        &missing_message,
+    )
+}
+
+fn gen_methods_inner(
+    field: &Field,
+    before: &[Field],
+    after: &[Field],
+    constructed: &TokenStream,
+    constructed_out_generics: &TokenStream,
+    bounded_generics: &TokenStream,
+    repeated_message: &str,
+    missing_message: &str,
 ) -> TokenStream {
     let before_generics = before.iter().map(|field| &field.generic);
     let after_generics = after.iter().map(|field| &field.generic);
@@ -213,7 +187,7 @@ fn gen_comp_methods(
         let (#(#destructured_before,)* (_,), #(#destructured_after,)*) = self.0;
     };
 
-    let constructed = {
+    let construction = {
         let iter = before
             .iter()
             .chain(Some(field))
@@ -224,14 +198,6 @@ fn gen_comp_methods(
             Builder((#(#iter,)*))
         }
     };
-
-    let var_name = key
-        .name
-        .strip_prefix("comp_")
-        .expect("components keys must start with var_");
-
-    let repeated_message = format!("Repeated component {var_name}");
-    let missing_message = format!("Missing component {var_name}");
 
     let right_generics = {
         let (before_generics, after_generics) = (before_generics.clone(), after_generics.clone());
@@ -259,28 +225,25 @@ fn gen_comp_methods(
 
     let constructed_out_generics = {
         quote! {
-            ( #(#before_generics,)* ((#key_generic, core::marker::PhantomData<#into_view>),), #(#after_generics,)*)
+            ( #(#before_generics,)* (#constructed_out_generics,), #(#after_generics,)*)
         }
     };
 
-    let [bounded_generics, into_view_bounded_generics] =
-        VarOrComp::get_bounded_comp_generics(&field.generic, into_view, self_closed);
-
     quote! {
         impl<#generics> Builder<#right_generics> {
-            pub fn #key<#bounded_generics, #into_view_bounded_generics>(self, #key: #key_generic) -> Builder<#constructed_out_generics> {
-                let #key = ((#key, core::marker::PhantomData),);
+            pub fn #key<#bounded_generics>(self, #key: #key_generic) -> Builder<#constructed_out_generics> {
+                let #key = (#constructed,);
                 #destructured
-                #constructed
+                #construction
             }
         }
 
         impl<__Dup__, #generics> Builder<#dup_right_generics> {
             #[deprecated(note = #repeated_message)]
-            pub fn #key<#bounded_generics, #into_view_bounded_generics>(self, #key: #key_generic) -> Builder<#constructed_out_generics> {
-                let #key = ((#key, core::marker::PhantomData),);
+            pub fn #key<#bounded_generics>(self, #key: #key_generic) -> Builder<#constructed_out_generics> {
+                let #key = (#constructed,);
                 #destructured_dup
-                #constructed
+                #construction
             }
         }
 
